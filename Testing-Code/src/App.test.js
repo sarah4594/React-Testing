abe737df86be33c2e3a40507015d48983d0627b1
sarah4594 <sarah4594@gmail.com>
2019-09-10 14:44:32 -0500

    ✅Write integration test

--- Testing-Code/src/App.test.js
+++ Testing-Code/src/App.test.js
@@ -1,7 +1,12 @@
-import { add } from './App'
+import { add, total } from './App'
 
 test('add', () => {
   const value = add(1, 2)
   expect(value).toBe(3)
   expect(add(5, 2)).toBe(7)
+})
+
+// Integration Test - testing functions that rely on other functions
+test('total', () => {
+  expect(total(5, 20)).toBe('$25')
 })
