2f98922ddacfed781f86ee9fac50b16a72c74a7b
sarah4594 <sarah4594@gmail.com>
2019-09-10 15:08:27 -0500

    ✅Test with a mocked dependency

--- Testing-Code/src/App.test.js
+++ Testing-Code/src/App.test.js
@@ -1,6 +1,17 @@
 import { total } from './App'
+import { add } from './add'
+
+jest.mock('./add', () => ({
+  add: jest.fn(() => 25),
+}))
 
 // Integration Test - testing functions that rely on other functions
 test('total', () => {
   expect(total(5, 20)).toBe('$25')
+  expect(add).toHaveBeenCalledTimes(1)
+
+  add.mockImplementation(() => 30)
+
+  expect(total(5, 25)).toBe('$30')
+  expect(add).toHaveBeenCalledTimes(2)
 })
