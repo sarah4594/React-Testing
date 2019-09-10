7a35879c9c30cdfcd823978fdda3e47117f2de20
sarah4594 <sarah4594@gmail.com>
2019-09-10 14:51:41 -0500

    ✅Test with mocked function

--- Testing-Code/src/App.test.js
+++ Testing-Code/src/App.test.js
@@ -1,12 +1,16 @@
-import { add, total } from './App'
+import { total } from './App'
+
+const add = jest.fn(() => 7)
 
 test('add', () => {
-  const value = add(1, 2)
-  expect(value).toBe(3)
+  // const value = add(1, 2)
+  // expect(value).toBe(3)
   expect(add(5, 2)).toBe(7)
+  expect(add).toHaveBeenCalledTimes(1)
+  expect(add).toHaveBeenCalledWith(5, 2)
 })
 
 // Integration Test - testing functions that rely on other functions
-test('total', () => {
-  expect(total(5, 20)).toBe('$25')
-})
+// test('total', () => {
+//   expect(total(5, 20)).toBe('$25')
+// })
