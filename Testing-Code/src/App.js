f7bc1a1b5e1ff74c2a85e507e0740550f6202fee
sarah4594 <sarah4594@gmail.com>
2019-09-11 12:47:28 -0500

    🎉Start actual app!

--- Testing-Code/src/App.js
+++ Testing-Code/src/App.js
@@ -1,5 +1,26 @@
-import { add } from './add'
+/* eslint react/no-did-mount-set-state: 0 */
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
 
-export const total = (shipping, subTotal) => {
-  return '$' + add(shipping, subTotal)
-}
+import MoviesList from './MoviesList'
+import MovieDetail from './MovieDetail'
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+export default App
